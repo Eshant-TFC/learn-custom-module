d87c82462eefbad86aeedeeed9fc07f559d2c314
Eshant Arora <eshant.arora@hashicorp.com>
2025-06-11 12:54:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,19 @@
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+  }
+}
+
 module "custom_module" {
   source  = "app.terraform.io/eshant-hcp-tf-test/Testing/aws"
   version = "1.0.0"
+}
+
+resource "null_resource" "example" {
+  provisioner "local-exec" {
+    command = "echo 'This is a test null resource'"
+  }
 }
